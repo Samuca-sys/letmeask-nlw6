8ecd2afccef577a36d1b9143c9241d71b51bede5
Samuel Macedo <smauel.macfer@gmail.com>
2021-06-23 07:06:34 -0300

    ci: add Auth Context into App.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,27 @@
-import { useHistory } from 'react-router-dom'
+import { useContext } from 'react';
+
+import { useHistory } from 'react-router-dom';
+
+import { AuthContext } from '../App';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
-import { Button } from '../components/Button'
+import { Button } from '../components/Button';
 
-import "../styles/auth.scss"
+import "../styles/auth.scss";
 
 export function Home() {
   const history = useHistory();
+  const { signInWithGoogle, user } = useContext(AuthContext);
 
-  function navigateToNewRoom() {
-    history.push('/rooms/new')
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+    history.push('/rooms/new');
   }
-
 
   return (
     <div id='page-auth'>
@@ -28,7 +35,7 @@ export function Home() {
         <div className="main-content">
           <img src={logoImg} alt='Letmeask' />
 
-          <button onClick={navigateToNewRoom} className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt='Logo do Google' />
             Crie sua sala com o Google
           </button>
