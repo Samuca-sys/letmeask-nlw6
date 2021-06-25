07dd3a62d0c5e7aaf732b0ce124439279c642536
Samuel Macedo <smauel.macfer@gmail.com>
2021-06-25 08:10:56 -0300

    feat: delete questions as admin

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -4,10 +4,13 @@ import {useParams} from 'react-router-dom';
 import { useRoom } from '../hooks/useRoom';
 
 import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
 import { Question } from '../components/Question';
+
+import { database } from '../services/firebase';
 
 import '../styles/room.scss';
 
@@ -23,6 +26,12 @@ export function AdminRoom() {
 
   const {title, questions} = useRoom(roomId);
   // const {user} = useAuth();
+
+  async function handleDeleteQuestion(questionId: string) {
+    if(window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
   return(
     <div id="page-room">
       <header>
@@ -54,7 +63,14 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
-              />
+              >
+                <button
+                  type="button"
+                  onClick={()=> handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover pergunta"/>
+                </button>  
+              </Question>
             )
           })}
         </div>
